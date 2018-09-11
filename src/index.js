597fce95e324b4193e50577487810dcf7b9897a4
poyuKiryl <poyuki9@gmail.com>
2018-09-11 11:22:31 +0300

    sorter task

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
 class Sorter {
+  comparator(a,b){
+    return a-b;
+  }
   constructor() {
-    // your implementation
+    this.array=[];
   }
 
   add(element) {
     // your implementation
+      this.array.push(element);
   }
 
   at(index) {
     // your implementation
+      return this.array[index]
   }
 
   get length() {
     // your implementation
+      return this.array.length
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+      indices.sort((a,b)=>a-b);
+      let tempArray=[];
+      indices.forEach((el)=>{
+        tempArray.push(this.array[el]);
+      });
+      tempArray.sort(this.comparator);
+      indices.forEach((el,index)=>{
+        this.array[el]=tempArray[index];
+      })
   }
 
   setComparator(compareFunction) {
-    // your implementation
+      this.comparator=compareFunction
   }
 }
 
